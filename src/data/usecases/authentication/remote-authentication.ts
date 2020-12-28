bd6d089954459b69f93263d366a62e102d38dc2a
Brendo Vale <brendo.vale02@gmail.com>
2020-12-28 02:53:15 -0400

    feat: ensure RemoteAuthentication throws UnexpectedError if HttpPostClient returns 400

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,5 +1,6 @@
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
+import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpPostClient } from './remote-authentication-protocols'
 
@@ -16,7 +17,9 @@ export class RemoteAuthentication {
     })
 
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+      default: throw new UnexpectedError()
     }
   }
 }
