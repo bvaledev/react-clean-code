03b2a88e4dfa3a8aaad3b4e4362084d0fb1ddc5a
Brendo Vale <brendo.vale02@gmail.com>
2020-12-28 22:18:53 -0400

    test: ensure LoginForm start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,5 +9,11 @@ describe('Login Page', () => {
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = getByTestId('submit-btn') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+    const emailStatus = getByTestId('email-status') as HTMLButtonElement
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('❌')
+    const passwordStatus = getByTestId('password-status') as HTMLButtonElement
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('❌')
   })
 })
