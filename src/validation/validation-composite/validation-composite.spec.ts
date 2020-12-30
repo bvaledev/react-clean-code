8ecd7acdbac2b22a01effe7fc5feda9123a99646
Brendo Vale <brendo.vale02@gmail.com>
2020-12-30 07:16:00 -0400

    refactor: move sut creation to a factory method

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -1,30 +1,28 @@
 import { ValidationComposite } from './validation-composite'
 import { FieldValidationSpy } from '@/validation/test'
+import { FieldValidation } from '../protocols'
 
+const makeValidators = (error: Error = null): FieldValidation[] => {
+  const fieldValidationSpy = new FieldValidationSpy('any_field')
+  const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+  fieldValidationSpy2.error = error
+
+  return [
+    fieldValidationSpy,
+    fieldValidationSpy2
+  ]
+}
+
+const makeSut = (validators: FieldValidation[]): ValidationComposite => new ValidationComposite(validators)
 describe('Validation Composite', () => {
   test('Should return error if any validation return an error', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_error_message')
-
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2
-    ])
-
+    const sut = makeSut(makeValidators(new Error('any_error_message')))
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('any_error_message')
   })
 
   test('Should return falsy if any validation return falsy', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2
-    ])
-
+    const sut = makeSut(makeValidators())
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBeFalsy()
   })
