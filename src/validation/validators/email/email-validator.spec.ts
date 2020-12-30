e2263216632325cf81892a79a61c3854902f5ad6
Brendo Vale <brendo.vale02@gmail.com>
2020-12-30 06:13:51 -0400

    test: ensure EmailValidator return error if email is invalid

---
+++ src/validation/validators/email/email-validator.spec.ts
@@ -0,0 +1,11 @@
+import { InvalidFieldError } from '@/validation/error/invalid-field-error'
+import faker from 'faker'
+import { EmailValidator } from './email-validator'
+
+describe('Email Validator', () => {
+  test('Should return error if email is invalid', () => {
+    const sut = new EmailValidator('email')
+    const error = sut.validate('')
+    expect(error).toEqual(new InvalidFieldError('email'))
+  })
+})
