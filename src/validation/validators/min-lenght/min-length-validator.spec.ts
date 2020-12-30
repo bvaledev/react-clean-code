00124fde5fbd2de5cd04805fa67c332f0dedcacb
Brendo Vale <brendo.vale02@gmail.com>
2020-12-30 06:46:33 -0400

    test: ensure  MinLength Validator return falsy if min lenght is reached

--- src/validation/validators/min-lenght/min-length-validator.spec.ts
+++ src/validation/validators/min-lenght/min-length-validator.spec.ts
@@ -2,17 +2,17 @@ import { MinLenghtFieldError } from '@/validation/error/min-lenght-field-error'
 import faker from 'faker'
 import { MinLengthValidator } from './min-length-validator'
 
-const makeSut = (): MinLengthValidator => new MinLengthValidator('password', 8)
+const makeSut = (size: number): MinLengthValidator => new MinLengthValidator('password', size)
 
 describe('MinLength Validator', () => {
   test('Shoud return erro if min lenght is not reached', () => {
-    const sut = makeSut()
+    const sut = makeSut(8)
     const error = sut.validate(faker.internet.password(7))
     expect(error).toEqual(new MinLenghtFieldError('password', 8))
   })
 
   test('Shoud return falsy if min lenght is reached', () => {
-    const sut = makeSut()
+    const sut = makeSut(8)
     const error = sut.validate(faker.internet.password(8))
     expect(error).toBeFalsy()
   })
