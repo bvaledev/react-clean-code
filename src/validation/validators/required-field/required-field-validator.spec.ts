7139715396bff6ff5cd9070890fe04a743b65bec
Brendo Vale <brendo.vale02@gmail.com>
2020-12-30 06:23:14 -0400

    refactor: simplify sut creation

--- src/validation/validators/required-field/required-field-validator.spec.ts
+++ src/validation/validators/required-field/required-field-validator.spec.ts
@@ -2,10 +2,8 @@ import { RequiredFieldError } from '../../error'
 import { RequiredFieldValidator } from './required-field-validator'
 import faker from 'faker'
 
-const makeSut = (): RequiredFieldValidator => {
-  const sut = new RequiredFieldValidator(faker.database.column())
-  return sut
-}
+const makeSut = (): RequiredFieldValidator => new RequiredFieldValidator(faker.database.column())
+
 describe('RequiredField Validator', () => {
   test('Should return error if field is empty', () => {
     const sut = makeSut()
