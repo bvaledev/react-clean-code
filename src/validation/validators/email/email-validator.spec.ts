b975380b31565901363b7ca60baa77141fe9544d
Brendo Vale <brendo.vale02@gmail.com>
2020-12-30 06:51:18 -0400

    fix: ensure EmailValidator return falsy if email empty

--- src/validation/validators/email/email-validator.spec.ts
+++ src/validation/validators/email/email-validator.spec.ts
@@ -7,13 +7,19 @@ const makeSut = (): EmailValidator => new EmailValidator('email')
 describe('Email Validator', () => {
   test('Should return error if email is invalid', () => {
     const sut = makeSut()
-    const error = sut.validate('')
+    const error = sut.validate('invalid@email')
     expect(error).toEqual(new InvalidFieldError('email'))
   })
 
   test('Should return falsy if email is valid', () => {
     const sut = makeSut()
     const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
+
+  test('Should return falsy if email empty', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
     expect(error).toBeFalsy()
   })
 })
