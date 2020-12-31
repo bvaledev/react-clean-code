e5049448d3435fb48b7e87cb3d2d3b5e3d389ae2
Brendo Vale <brendo.vale02@gmail.com>
2020-12-31 11:09:13 -0400

    feat: make login save access token factory

--- src/main/factory/pages/login/login-factory.tsx
+++ src/main/factory/pages/login/login-factory.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Login } from '@/presentation/pages'
 import { makeRemoteAuthentication } from '../../usecases/authentication/remote-authentication-factory'
 import { makeLoginValidationFactory } from './login-validation-factory'
+import { makeSaveAccessTokenFactory } from '@/main/factory/usecases/save-access-token/save-access-token-factory'
 
 export const makeLogin: React.FC = () => {
   return (
     <Login
       validation={makeLoginValidationFactory()}
       authentication={makeRemoteAuthentication()}
+      saveAccessToken={makeSaveAccessTokenFactory()}
     />
   )
 }
